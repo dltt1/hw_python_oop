5714af412eea608cdf6878f5d3a0e37d1ed3a09f
Дмитрий Шапченко <dmitryshapchenko@yandex.ru>
2022-07-07 09:53:42 +0300

    flake8 change_2

--- homework.py
+++ homework.py
@@ -71,9 +71,9 @@ class Running(Training):
         super().__init__(action, duration, weight)
 
     def get_spent_calories(self) -> float:
-        calories_run = (self.coeff_calorie_1 * self.get_mean_speed() -
-                        self.coeff_calorie_2) * self.weight / self.M_IN_KM * (
-                        self.duration * 60)
+        calories_run = (self.coeff_calorie_1 * self.get_mean_speed()
+                        - self.coeff_calorie_2) * self.weight / self.M_IN_KM \
+                        * (self.duration * 60)
         return calories_run
 
 
@@ -90,8 +90,8 @@ class SportsWalking(Training):
         self.height = height
 
     def get_spent_calories(self) -> float:
-        calories_walk = (self.coeff_walk_1 * self.weight +
-                         (self.get_mean_speed() ** 2 // self.height)
+        calories_walk = (self.coeff_walk_1 * self.weight
+                         + (self.get_mean_speed() ** 2 // self.height)
                          * self.coeff_walk_2 * self.weight) \
                          * (self.duration * 60)
         return calories_walk
